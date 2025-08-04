2df64750ff168b7ab8f5563b84a2cf2f00577da4
kameshNethsara <kamesh317nethsara@gmail.com>
2025-08-04 10:30:35 +0530

    update

--- src/main/java/com/example/spbootdeployment/controller/CustomerController.java
+++ src/main/java/com/example/spbootdeployment/controller/CustomerController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @GetMapping
     public String getCustomers() {
-        return "List of customers";
+        return "List of customersjj";
     }
 }
