3c25072c7d732250fac5699a2e73792777ecfa2a
kameshNethsara <kamesh317nethsara@gmail.com>
2025-08-04 10:03:08 +0530

    Initial commit

---
+++ src/main/java/com/example/spbootdeployment/SpbootdeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.spbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootdeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbootdeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spbootdeployment/SpbootdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spbootdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootdeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
